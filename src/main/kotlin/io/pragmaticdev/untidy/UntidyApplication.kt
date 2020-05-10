ae2609b430243b292afe4014246d9f03d3baf075
Cole Mackenzie <colemackenzie1@gmail.com>
2020-05-10 13:42:40 -0700

    Initial commit

---
+++ src/main/kotlin/io/pragmaticdev/untidy/UntidyApplication.kt
@@ -0,0 +1,11 @@
+package io.pragmaticdev.untidy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UntidyApplication
+
+fun main(args: Array<String>) {
+	runApplication<UntidyApplication>(*args)
+}
---
+++ src/test/kotlin/io/pragmaticdev/untidy/UntidyApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.pragmaticdev.untidy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UntidyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
