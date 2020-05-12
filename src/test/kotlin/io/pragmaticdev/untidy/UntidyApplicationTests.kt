ed0a0af8cd42ea1748377ac5cc4767e3c93ed802
Cole Mackenzie <colemackenzie1@gmail.com>
2020-05-11 21:48:26 -0700

    Loading embedded cassandra before spring context loads

--- src/test/kotlin/io/pragmaticdev/untidy/UntidyApplicationTests.kt
+++ src/test/kotlin/io/pragmaticdev/untidy/UntidyApplicationTests.kt
@@ -1,11 +1,19 @@
 package io.pragmaticdev.untidy
 
+import org.cassandraunit.spring.CassandraDataSet
+import org.cassandraunit.spring.CassandraUnit
+import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
 
 
+@CassandraDataSet(keyspace = "unitTest", value = ["cql/dataset.cql"])
+@CassandraUnit
+@TestExecutionListeners(value = [CassandraUnitDependencyInjectionTestExecutionListener::class, DependencyInjectionTestExecutionListener::class])
 @SpringBootTest
-class UntidyApplicationTests  {
+class UntidyApplicationTests {
 
     @Test
     fun contextLoads() {
